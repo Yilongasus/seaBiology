4e031f8f4ab99791a02c341590f024cb7f632849
JingYi <zmmouoasus@gmail.com>
2024-04-13 15:33:31 +0800

    ✨ ：首頁
    1. 加上圖片。

--- sea_biology/src/components/TheIndex.vue
+++ sea_biology/src/components/TheIndex.vue
@@ -9,12 +9,51 @@
     <div id="main" class="mt-4 p-5 position-sticky zindex_2 text-white">
         <h1 class="text-light pt-5 shack display-2 z-3" :key="text">{{ text }}</h1>
     </div>
+    <div class="d-flex justify-content-center position-sticky zindex_2 mt-3">
+        <div class="imgIndexShow imgLeftRect animate__animated animate__fadeInLeft">
+            <img
+            class="img-thumbnail w-50" 
+            src="https://ws.nmmba.gov.tw/001/Upload/OldFile/FileDownLoad/PublishActivies/20150715104349228.JPG"
+            alt="費雪的秘密海報"
+            >
+        </div>
+        <!-- <div class="imgIndexShow imgCenterRect">
+            <img
+            class="img-thumbnail h-50"
+            src="https://ws.nmmba.gov.tw/001/Upload/OldFile/FileDownLoad/PublishActivies/20131104092709371.jpg"
+            alt="蟹逅經典特展海報"
+            >
+        </div> -->
+        <div class="imgIndexShow imgRightRect animate__animated animate__fadeInRightBig">
+            <img
+            class="img-thumbnail w-50"
+            src="https://ws.nmmba.gov.tw/001/Upload/217/relpic/6415/12438/fb37bbcf-a02f-4508-a881-a0d8802238b7.jpg"
+            alt="這樣教我就懂海報"
+            >
+        </div>
+    </div>
 </template>
 <style>
     .shack {
         font-size: large;
         z-index: 4;
         animation: change 1s ease-in-out;
+    }
+    .imgLeftRect {
+        background: linear-gradient(to right, #083DA6, #3971E0);
+        border-radius: 16% 84% 77% 23% / 41% 67% 33% 59%;
+    }
+    .imgLeftRect img {
+        box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+        transform:rotate(-5deg);
+    }
+    .imgRightRect {
+        background: linear-gradient(to right, #8093F1, #FBC860);
+        border-radius: 37% 63% 45% 55% / 31% 28% 72% 69%;
+    }
+    .imgRightRect img {
+        box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+        transform:rotate(5deg);
     }
     @keyframes change {
         0% {
--- sea_biology/src/components/TheLogin.vue
+++ sea_biology/src/components/TheLogin.vue
@@ -8,17 +8,18 @@
         <p class="text-white">
         {{ item }}
         </p>
-        <p class="text-warning shack" :key="text">{{ text }}</p>
+
         <div v-for="(value, key, index) in data" :key="index">
             <p>索引 {{ index }}</p>
             <p>key {{ key }}</p>
             <p>value {{ value }}</p>
         </div>
+        <input type="text" v-model="watchVal.level.a"/>
+        <input type="text" v-model="watchVal.level.b"/>
     </div>
 </template>
 <script setup>
     import { ref, reactive, watch } from 'vue'
-    // eslint-disable-next-line no-undef
     let item = reactive({
             name:2,
             info: {
@@ -32,9 +33,9 @@
             c: 'candy'
         })
 
-    watch(item, watchItem => {
-        console.log("reactive:", watchItem);
-    })
+    // watch(item, watchItem => {
+    //     console.log("reactive:", watchItem);
+    // })
 
     let changeValue = () => {
         changItem.value > 2 ? item.info.age = 4 : item.info.age = 1
@@ -43,6 +44,17 @@
     setTimeout(()=> {
         text.value = '歡迎來到水世界'
     }, 3000)
+
+    let watchVal = ref({
+        level: {
+            a: 'apple',
+            b: 'banana',
+        }
+    })
+
+    watch([()=>watchVal.value.level.a, ()=>watchVal.value.level.b], (newVal, old) => {
+        console.log(newVal, old);
+    });
 </script>
 <style scoped>
     .shack {
@@ -57,6 +69,14 @@
         100% {
             opacity: 1;
             transform: translateY(0);
+        }
+    }
+    #articleWidth {
+        width: 25%;
+    }
+    @media screen and (max-width:768px) {
+        #articleWidth {
+        width: 100%;
         }
     }
 </style>
