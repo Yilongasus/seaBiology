f926c965c89e5fab63ebb0cb11aca95991711744
JingYi <zmmouoasus@gmail.com>
2024-07-16 22:22:13 +0800

    💄：調整展館消息layout排版

--- sea_biology/src/components/TheNews.vue
+++ sea_biology/src/components/TheNews.vue
@@ -8,11 +8,9 @@
 
 <template>
   <div id="main" class="mt-4 p-5 position-sticky zindex_2 text-white">
-    <h1 class="mb-3">展館消息</h1>
-    <div v-if="seaData">
-      <!-- <h3>展館公告</h3> -->
+    <h1 class="mb-3 fw-semibold text-center display-4">展館消息</h1>
+    <!-- <div v-if="seaData">
       <div v-for="(data, index) in seaData" :key="index" class="border-orange-800">
-
         <div class="row newsColor news bg-light bg-gradient rounded-pill text-base w-75 w-75 mx-auto my-3">
           <div class="col-12 py-4">
             <h5 class="fs-4 px-5">{{ data['標題'] }}</h5>
@@ -27,12 +25,37 @@
             <a class="btn text-decoration-none btn-yellow text-black" :href="data['Source']">按我前往➡</a>
           </div>
         </div>
-
       </div>
-    </div>
+    </div> -->
 
-    <div v-else>
-      <h3 class="text-white" data-aos="zoom-out-left">載入中...</h3>
+
+    <div class="container mt-1 pt-3">
+        <div v-if="seaData" class="pb-4 d-grid">
+          <div v-for="(data, index) in seaData" :key="index" class=" rounded-lg position-relative overflow-hidden">
+              <a :href="data['Source']" class="fs-6 d-block py-2 position-relative overflow-hidden group text-decoration-none">
+                  <div class="d-flex flex-row justify-content-between align-items-center px-5 py-3 text-center rounded" style="background: linear-gradient(to right, white, #EAEAEA); box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);">
+                      <p class="col-span-2 fs-5 fw-semibold pt-3" style="color: #163464;">{{ data['標題'] }}</p>
+                      <div class="d-flex flex-column justify-content-center">
+                        <p class="fs-6 text-start col-span-2" style="color: #756C63;">發佈日期 <span class="badge text-wrap">{{ data['上版日期'] }}</span></p>
+                        <p class="fs-6 text-start col-span-2" style="color: #756C63;">資料來源 <span class="badge text-wrap">{{ data['資料來源'] }}</span></p>
+
+                      </div>
+                  </div>
+                  <div class="position-absolute bottom-0 start-0 end-0 top-0 h-100 w-100 overflow-hidden bg-black opacity-50 d-none werd-block"></div>
+                  <div class="position-absolute bottom-0 start-0 end-0 top-0 d-flex align-items-center justify-content-center d-none werd-flex">
+                      <span class="bg-primary text-white px-5 py-3 rounded hover-text-primary hover-bg-light fs-5">
+                          前往查看
+                          <el-icon size="16">
+                              <elIconTopRight />
+                          </el-icon>
+                      </span>
+                  </div>
+              </a>
+          </div>
+        </div>
+        <div v-else>
+          <h3 class="text-white" data-aos="zoom-out-left">載入中...</h3>
+        </div>
     </div>
 
   </div>
@@ -89,7 +112,13 @@
 .newsColor {
   color: #2c3e50;
 }
+.hover-container:hover .hover-overlay {
+    display: block;
+}
 
+.hover-overlay {
+    display: none;
+}
 .news {
   box-shadow: 5px 5px 0px rgba(184, 204, 244, 0.8);
   transition: all 0.5s;
@@ -100,7 +129,22 @@
   box-shadow: 5px 5px 0px rgba(162, 180, 216, 0.8);
 }
 
+.badge {
+  background: linear-gradient(217deg, #6B5264, rgba(255,0,0,0) 70.71%),
+            linear-gradient(127deg, #154166, rgba(0,255,0,0) 70.71%),
+            linear-gradient(336deg, #6B5264, rgba(82, 82, 82, 0) 70.71%);
+}
+
 #main {
   border-radius: 80% 20% 81% 19% / 4% 62% 18% 50%;
+}
+
+werd-flex:hover {
+  display: flex;
+}
+
+
+werd-block:hover {
+  display: block;
 }
 </style>
